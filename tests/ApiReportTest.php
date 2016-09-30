e1d3e28e048b8b0e8d64e2e477ab3888d795d8d2
Pavel Polshkov <pavel.polshkov@gmail.com>
2016-09-30 15:34:11 +0300

    PHPCS cleanup

--- tests/ApiReportTest.php
+++ tests/ApiReportTest.php
@@ -1,31 +1,40 @@
 <?php
 
-class ApiReportTest extends TestCase {
+class ApiReportTest extends TestCase
+{
 
-    public function testNoKeyReport() {
+    public function testNoKeyReport() 
+    {
         $response = $this->call('POST', '/api/report');
 
         $this->assertEquals(400, $response->getStatusCode());
     }
 
-    public function testNoContentReport() {
-        $response = $this->call('POST', '/api/report', [
+    public function testNoContentReport() 
+    {
+        $response = $this->call(
+            'POST', '/api/report', [
             'api_key' => $this->_getProject()->api_key
-        ]);
+            ]
+        );
 
         $this->assertEquals(422, $response->getStatusCode());
     }
 
-    public function testAddReport() {
-        $response = $this->call('POST', '/api/report', [
+    public function testAddReport() 
+    {
+        $response = $this->call(
+            'POST', '/api/report', [
             'api_key' => $this->_getProject()->api_key,
             'content' => 'Test error'
-        ]);
+            ]
+        );
         \Log::info($response->getContent());
         $this->assertEquals(200, $response->getStatusCode());
     }
 
-    private function _getProject() {
+    private function _getProject() 
+    {
         return \App\Models\Project::first();
     }
 
